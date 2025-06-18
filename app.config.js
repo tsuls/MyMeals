a44435a20f5bf9bddb4bfb4fdbafdca72c48bd1e
tsuls <tsulsenti60@gmail.com>
2025-06-18 18:12:58 -0400

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
     name: 'MyMeals',
     slug: 'MyMeals',
     extra: {
-      openaiApiKey: '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', // your actual key
+      openaiApiKey: '', // your actual key
     },
   },
 };
